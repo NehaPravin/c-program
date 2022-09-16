efa84cdd2a536dde63f684bca871d5aa17dfd4ca
Neha Pravin <104181302+NehaPravin@users.noreply.github.com>
2022-09-15 20:18:07 -1200

    Add files via upload

---
+++ 11.1to5.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("\n");
+	for(i=1;i<=5;i++)
+	{
+		printf("%d \n",i);
+	}
+	printf("\n");
+	for(i=1;i<=9;i+=2)
+	{
+		printf("%d\n ",i);
+	}
+	printf("\n");
+	for(i=5;i>=1;i--)
+	{
+		printf("%d\n",i);
+	}
+}
