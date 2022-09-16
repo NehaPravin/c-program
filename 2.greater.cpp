a2b8374cc88611c6ed483e688a97366c3da66a8d
Neha Pravin <104181302+NehaPravin@users.noreply.github.com>
2022-09-15 20:14:55 -1200

    Add files via upload

---
+++ 2.greater.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the value of a and b");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	printf("greater no is %d",a);
+	else
+	printf("greater no is %d",b);
+}
